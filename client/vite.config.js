76fe586435dfc586c419bc51127f20210af5145c
ShahTaimoor <taimour448@gmail.com>
2025-06-21 13:03:59 +0500

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,32 +9,35 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      filename: 'manifest.webmanifest',
-      registerType: "autoUpdate",
-      includeAssets: ["vite.svg", "robots.txt", "logos.png"],
-      manifest: {
-        name: "Gul Autos",
-        short_name: "Gul Autos",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "maskable.png",
-            sizes: "196x196",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-          {
-            src: "logos.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-      
-        ]
+  registerType: "autoUpdate",
+  filename: 'manifest.webmanifest',
+  includeAssets: ["vite.svg", "robots.txt", "logos.png"],
+  manifest: {
+    name: "Gul Autos",
+    short_name: "Gul Autos",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "maskable.png",
+        sizes: "196x196",
+        type: "image/png",
+        purpose: "any maskable"
+      },
+      {
+        src: "logos.png",
+        sizes: "192x192",
+        type: "image/png"
       }
-    }),
+    ]
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+  },
+})
+
   ],
   resolve: {
     alias: {
