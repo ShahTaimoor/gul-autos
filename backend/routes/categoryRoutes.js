afa727262ba4078bffd887101bb52acf729d3549
ShahTaimoor <taimour448@gmail.com>
2025-06-26 01:18:22 +0500

    s

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -118,7 +118,7 @@ router.delete('/delete-category/:slug', isAuthorized, isAdmin, async (req, res) 
     }
 });
 
-// Get all categorie
+// Get all categori
 router.get('/all-category', async (req, res) => {
     try {
         const categories = await Category.find().sort({ order: 1 });
