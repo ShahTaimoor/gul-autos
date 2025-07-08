76e7ee25d132c46852549eaebd798a618c6731c8
ShahTaimoor <taimour448@gmail.com>
2025-07-08 17:01:08 +0500

    s

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -44,9 +44,9 @@ router.post('/login', async (req, res) => {
 
     return res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-      maxAge: 365 * 24 * 60 * 60 * 1000,
+  secure: true,       
+  sameSite: "None",        
+  maxAge: 365 * 24 * 60 * 60 * 1000
     }).status(200).json({
       success: true,
       user,
