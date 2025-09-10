23db0a940c74b8975853946183f740548b8e7f78
ShahTaimoor <taimour448@gmail.com>
2025-09-10 19:47:21 +0500

    c

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { isAuthorized, isAdmin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
